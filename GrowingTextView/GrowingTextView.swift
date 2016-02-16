f27136e56ee1fff292d4af4aa3f4fbf1ff9fa3b4
Xin Hong <hongxinhope@gmail.com>
2016-02-16 13:31:24 +0800

    Initial Commit

---
+++ GrowingTextView/GrowingTextView.swift
@@ -0,0 +1,13 @@
+//
+//  GrowingTextView.swift
+//  GrowingTextView
+//
+//  Created by Xin Hong on 16/2/16.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class GrowingTextView: UIScrollView {
+
+}
---
+++ GrowingTextViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrowingTextViewExample
+//
+//  Created by 洪鑫 on 16/2/16.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
